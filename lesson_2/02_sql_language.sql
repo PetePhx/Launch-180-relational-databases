ab69f71297c0d6bfff0c78c6d353e8371c85754b
PetePhx <pete0phx@yandex.com>
2018-03-28 11:50:15 -0700

    sql language summary

---
+++ lesson_2/02_sql_language.sql
@@ -0,0 +1,32 @@
+1. What kind of programming language is SQL?
+
+  A special-purpose, declarative language used for interacting with relational
+  databases.
+
+2. What are the three sublanguages of SQL?
+
+  Data Definition Language
+  Data Manipulation Language
+  Data Control Langugage
+
+3. Write the following values as quoted string values that could be used in a
+SQL query.
+
+-- canoe
+  'canoe'
+-- a long road
+  'a long road'
+-- weren't
+  'were''nt'
+-- "No way!"
+  '"No way!"'
+
+4. What operator is used to concatenate strings?
+
+5. What function returns a lowercased version of a string? Write a SQL statement using it.
+
+6. How does the psql console display true and false values?
+
+7. The surface area of a sphere is calculated using the formula A = 4π r2, where A is the surface area and r is the radius of the sphere.
+
+8. Use SQL to compute the surface area of a sphere with a radius of 26.3cm, truncated to return an integer.
