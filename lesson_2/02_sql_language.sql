1fcd723c7d2e3d15182d7aee095a86ff8542a9bb
PetePhx <pete0phx@yandex.com>
2018-03-29 11:16:38 -0700

    minor update

--- lesson_2/02_sql_language.sql
+++ lesson_2/02_sql_language.sql
@@ -23,10 +23,45 @@ SQL query.
 
 4. What operator is used to concatenate strings?
 
-5. What function returns a lowercased version of a string? Write a SQL statement using it.
+  ||
+
+  SELECT 'a' || 'b' ;
+   ?column?
+  ----------
+   ab
+  (1 row)
+
+5. What function returns a lowercased version of a string? Write a SQL statement
+using it.
+
+  lower
+
+  SELECT lower('HeLlO tHeRe');
+    lower
+  -------------
+  hello there
+  (1 row)
 
 6. How does the psql console display true and false values?
 
-7. The surface area of a sphere is calculated using the formula A = 4π r2, where A is the surface area and r is the radius of the sphere.
+  t and f, respectively:
 
-8. Use SQL to compute the surface area of a sphere with a radius of 26.3cm, truncated to return an integer.
+  SELECT true, false;
+   bool | bool
+  ------+------
+   t    | f
+  (1 row)
+
+
+7. The surface area of a sphere is calculated using the formula A = 4π r2, where
+A is the surface area and r is the radius of the sphere.
+
+Use SQL to compute the surface area of a sphere with a radius of 26.3cm,
+truncated to return an integer.
+
+  SELECT floor(4 * pi() * 26.3 ^ 2) AS "area (cm^2)";
+
+   area (cm^2)
+  -------------
+      8692
+  (1 row)
