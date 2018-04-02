5260206b1a2a20dad8877e2fd03b22ef704aab64
PetePhx <pete0phx@yandex.com>
2018-04-02 11:25:23 -0700

    levels of schema practice problems

---
+++ lesson_3/03_levels_of_schema.sql
@@ -0,0 +1,26 @@
+1. What are the three levels of schema?
+
+  * Conceptual
+
+  * Logical
+
+  * Physical
+
+2. What is a conceptual schema?
+
+  A high abstraction level design dealing with relationships between entities.
+
+3. What is a physical schema?
+
+  An implementation-specific design focused on actual tables/rows/relationships.
+
+4. What are the three types of relationships that can be shown in a database
+diagram?
+
+  Depending on the multiplicity of connections among the two entities:
+
+  * one-to-one
+
+  * one-to-many
+
+  * many-to-many
