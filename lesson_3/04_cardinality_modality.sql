2e2b4c5e1a8465165d82aa368b5e173dd2f8d0bb
PetePhx <pete0phx@yandex.com>
2018-04-02 11:58:35 -0700

    cardinality/modality practice problems

---
+++ lesson_3/04_cardinality_modality.sql
@@ -0,0 +1,17 @@
+1. What is cardinality?
+
+  An indicator for the number of instances on each side of a relationship
+
+2. What is modality?
+
+  It indicates the minimumm number of instances required on each side of a
+  relationship.
+
+3. If one side of a relationship has a modality of 1, what is the smallest
+number of instances that can be on that side of the relationship?
+
+  One.
+
+4. What type of notation was described in depth throughout most of the video?
+
+  Crows Foot notation.
